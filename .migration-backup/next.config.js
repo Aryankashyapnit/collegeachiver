a72c4fd584315e60e90c8dad9dc42ebfc45ba52c
aryAan kashyap <ak0543643@gmail.com>
2026-05-28 14:19:58 +0530

    Bypassed strict build errors

--- .migration-backup/next.config.js
+++ .migration-backup/next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // Ignore TypeScript build errors
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Ignore ESLint errors during builds
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
